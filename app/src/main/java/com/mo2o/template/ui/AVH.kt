5f1386da21529fb8c2cd70188473cbd28fdf8703
carbaj0 <carbaj03@gmail.com>
2017-08-07 22:15:33 +0200

    feat: Add generic adapter and Viewholder

---
+++ app/src/main/java/com/mo2o/template/ui/AVH.kt
@@ -0,0 +1,29 @@
+package com.mo2o.template.ui
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.mo2o.template.api.model.Repo
+import com.mo2o.template.inflate
+
+typealias RepoAdapter = AVH<RepoViewHolder, Repo>
+
+abstract class ViewHolder<in M>(view: View) : RecyclerView.ViewHolder(view) {
+    abstract fun bind(model: M)
+}
+
+open class AVH<VH : ViewHolder<M>, M>(
+        val items: List<M>,
+        val listener: (M) -> Unit,
+        val holder: (view: View) -> VH,
+        val layout: Int
+) : RecyclerView.Adapter<VH>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = holder(parent.inflate(layout))
+
+    override fun onBindViewHolder(holder: VH, position: Int) = with(holder) {
+        bind(items[position])
+        itemView.setOnClickListener { listener(items[position]) }
+    }
+
+    override fun getItemCount() = items.size
+}
