b07ab26281d88b8ca02918085aadb46c7d39a59c
alejandro <alejandro.carbajo@mo2o.com>
2017-08-08 13:54:12 +0200

    feat: Implements BottomNavigationDrawer

--- app/src/main/java/com/mo2o/template/ViewExtension.kt
+++ app/src/main/java/com/mo2o/template/ViewExtension.kt
@@ -4,4 +4,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 
 
+const val HOME = android.R.id.home
+const val OVERVIEW = R.id.bndOverview
+const val REPOSITORIES = R.id.bndRepositories
+const val STARS = R.id.bndStars
+const val FOLLOWING = R.id.bndFollowing
+
 infix fun ViewGroup.inflate(res: Int) = LayoutInflater.from(context).inflate(res, this, false)
+
+fun Action(f: () -> Unit): Boolean {
+    f()
+    return true
+}
--- app/src/main/java/com/mo2o/template/ui/MainActivity.kt
+++ app/src/main/java/com/mo2o/template/ui/MainActivity.kt
@@ -3,13 +3,14 @@ package com.mo2o.template.ui
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import com.mo2o.template.R
-import com.mo2o.template.loadFragment
+import com.mo2o.template.*
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.*
 import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
@@ -19,7 +20,16 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
         AndroidInjection.inject(this)
         setSupportActionBar(toolbar)
         setContentView(R.layout.activity_main)
-        showUser()
+
+        bnd.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                OVERVIEW -> Action { }
+                STARS -> Action { }
+                REPOSITORIES -> Action { showUser() }
+                FOLLOWING -> Action { }
+            }
+            true
+        }
     }
 
     private fun showUser() = loadFragment<MainFragment>()
